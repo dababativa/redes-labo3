da962a38091df6ac58efa5fbc1f16a00656867b5
Daniel Babativa <da.babativa@uniandes.edu.co>
2020-10-03 10:58:26 -0500

    DA: Logs server yahoooo

--- src/cliente/Server.java
+++ src/cliente/Server.java
@@ -3,6 +3,8 @@ package cliente;
 import java.io.*;
 import java.net.*;
 import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Server {
 	
@@ -33,8 +35,12 @@ public class Server {
 
 
 	public static void main(String[] args){
-		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH_mm_ss");  
+		LocalDateTime createDate = LocalDateTime.now(); 
+		String spaghetti = "log-server-"+dtf.format(createDate);
+		File log = new File("./logs/server/"+spaghetti+".txt");
 		Server babyServer = new Server(6969);
+		dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		System.out.println("Nueva instancia del servidor");
 		System.out.println("Esperando conexiones");
 		 FileInputStream fis;
@@ -42,6 +48,9 @@ public class Server {
 	        BufferedOutputStream out;
 	        byte[] buffer = new byte[8192];
 	        try {
+	        	FileWriter fw = new FileWriter(log, true);
+				fw.write("Se inicia el envio del archivo el " + dtf.format(LocalDateTime.now()) + "\n");
+				fw.write("Se enviará el archivo video.mp4"+ "\n");
 	        	Socket socket = serverCoket.accept();
 	        	System.out.println("Nueva conexión");
 	        	File chivito2 = new File("./docs/video.mp4");
@@ -49,17 +58,32 @@ public class Server {
 	            bis = new BufferedInputStream(fis);
 	            out = new BufferedOutputStream(socket.getOutputStream());
 	            int count;
+	            File chivaso = new File("./docs/test.mp4");
+				fw.write("El tamaño del archivo es: " + chivaso.length() + " bytes" + "\n");
 	            System.out.println("Enviando el archivo");
 	            int n = 0;
+	            long initio = System.currentTimeMillis();
 	            while ((count = bis.read(buffer)) > 0) {
 	                n++;
 	            	out.write(buffer, 0, count);
 
 	            }
+	            int finito = (int) (System.currentTimeMillis() - initio);
+				String tiempo = "";
+				if(finito<1000 && finito >99) {
+					tiempo = "0." + finito;
+				} else {
+					finito = finito/1000;
+					tiempo += finito;
+				}
+	           
+				fw.write("El archivo se envió con éxito en un tiempo de " + tiempo + " segundos" + "\n");
+				fw.write("Total de segmentos enviados: " + n + " segmentos" + "\n");
 	            System.out.println("Terminando la transferencia del archivo");
 	            out.close();
 	            fis.close();
 	            bis.close();
+	            fw.close();
 	            System.out.println("Apagando el servidor Salu2");
 	        } catch (IOException e) {
 	            e.printStackTrace();
